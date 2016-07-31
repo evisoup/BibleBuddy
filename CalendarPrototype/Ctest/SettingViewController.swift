41d30169d9c3c73deed04cfbb4e6618717b592d4
刘恒邑 <h225liu@uwaterloo.ca>
2016-07-30 20:12:06 -0400

    completed two date picker

--- CalendarPrototype/Ctest/SettingViewController.swift
+++ CalendarPrototype/Ctest/SettingViewController.swift
@@ -17,6 +17,10 @@ class SettingViewController: UIViewController, UITextFieldDelegate {
     @IBOutlet weak var textField_Date: UITextField!
 
     
+    @IBOutlet weak var endDate: UITextField!
+    
+    
+    
     var datePicker : UIDatePicker!
     
     func pickUpDate(textField : UITextField){
@@ -31,13 +35,22 @@ class SettingViewController: UIViewController, UITextFieldDelegate {
         let toolBar = UIToolbar()
         toolBar.barStyle = .Default
         toolBar.translucent = true
-        toolBar.tintColor = UIColor(red: 92/255, green: 216/255, blue: 255/255, alpha: 1)
+        //toolBar.tintColor = UIColor(red: 92/255, green: 216/255, blue: 255/255, alpha: 1)
+        toolBar.tintColor = UIColor.redColor()
         toolBar.sizeToFit()
         
         // Adding Button ToolBar
-        let doneButton = UIBarButtonItem(title: "Done", style: .Plain, target: self, action: "doneClick")
-        let spaceButton = UIBarButtonItem(barButtonSystemItem: .FlexibleSpace, target: nil, action: nil)
-        let cancelButton = UIBarButtonItem(title: "Cancel", style: .Plain, target: self, action: "cancelClick")
+
+        var doneButton = UIBarButtonItem(title: "Done", style: .Plain, target: self, action: #selector(SettingViewController.doneClick))
+        var spaceButton = UIBarButtonItem(barButtonSystemItem: .FlexibleSpace, target: nil, action: nil)
+        var cancelButton = UIBarButtonItem(title: "Cancel", style: .Plain, target: self, action: #selector(SettingViewController.cancelClick))
+        
+        if textField.tag == 2 {
+            toolBar.tintColor = UIColor.greenColor()
+             doneButton = UIBarButtonItem(title: "Done", style: .Plain, target: self, action: #selector(SettingViewController.doneClickB))
+             spaceButton = UIBarButtonItem(barButtonSystemItem: .FlexibleSpace, target: nil, action: nil)
+             cancelButton = UIBarButtonItem(title: "Cancel", style: .Plain, target: self, action: #selector(SettingViewController.cancelClickB))
+        }
         toolBar.setItems([cancelButton, spaceButton, doneButton], animated: false)
         toolBar.userInteractionEnabled = true
         textField.inputAccessoryView = toolBar
@@ -55,9 +68,26 @@ class SettingViewController: UIViewController, UITextFieldDelegate {
         textField_Date.resignFirstResponder()
     }
     
+    func doneClickB() {
+        let dateFormatter1 = NSDateFormatter()
+        dateFormatter1.dateStyle = .MediumStyle
+        dateFormatter1.timeStyle = .NoStyle
+        endDate.text = dateFormatter1.stringFromDate(datePicker.date)
+        endDate.resignFirstResponder()
+    }
+    func cancelClickB() {
+        endDate.resignFirstResponder()
+    }
+    
     
     func textFieldDidBeginEditing(textField: UITextField) {
-        self.pickUpDate(self.textField_Date)
+        if textField.tag == 1 {
+            self.pickUpDate(self.textField_Date)
+        } else if textField.tag == 2 {
+            self.pickUpDate(self.endDate)
+        }
+        
+
     }
     
     override func viewDidLoad() {
